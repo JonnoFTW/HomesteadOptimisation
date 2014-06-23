e4555fdc1932c884d42cdedbcf02ea0e4fa4d06c
Jonathan Mackenzie <jonmac1@gmail.com>
2014-06-23 17:28:21 +0930

    Added hill climbing methods,
    simulated annealing isn't written *yet*

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
 
 class Main {
 	private static double STEP = 0.01;
@@ -9,6 +12,7 @@ class Main {
 	private double minX = xPoints[0];
 	private double minY = yPoints[0];
 
+	private static Random rand = new Random();
 	/**
 	 * Setup the variables for the bounding rectangle
 	 */
@@ -27,7 +31,8 @@ class Main {
 	 *
 	 */
 	private class Point {
-		public double x, y;
+		public double x, y,starLength,equationLength;
+		public Point equationStart,equationEnd;
 
 		public Point(Double x, Double y) {
 			this.x = x;
@@ -52,13 +57,61 @@ class Main {
 		public double distanceToLine(Point p) {
 			return Math.sqrt(Math.pow(y - p.x * x + p.y, 2) / (p.x * p.x + 1));
 		}
-
+		/**
+		 * Find the length of cable required if this was a
+		 * candidate point using the star setup
+		 * @return the length of cable
+		 */
+		public double findStarLength() {
+			double length = 0;
+			for (int i = 0; i < xPoints.length; i++) {
+				length += distance(new Point(xPoints[i], yPoints[i]));
+			}
+			starLength = length;
+			return length;
+		}
+		/**
+		 * Find the length of cable required if this was a line
+		 * @return
+		 */
+		public double findEquationLength() {
+			double candidateTotalLength = 0;
+			ArrayList<Point> points = new ArrayList<Point>(5);
+			for (int i = 0; i < xPoints.length; i++) {
+				double x1 = xPoints[i];
+				double y1 = yPoints[i];
+				candidateTotalLength += new Point(x1, y1).distanceToLine(this);
+				// find where these perpendicular points meet the line
+				double m = x;
+				double c = y;
+				points.add(new Point((m * y1 + x1 - m*c) / (m*m + 1),(m*m * y1 + m*x1 + c) / (m*m + 1)));
+			}
+			// Add the greatest distance between the points
+			double lineLength = Double.MIN_VALUE;
+			for (Point point : points) {
+				for (Point point2 : points) {
+					if(point != point2) {
+						double dist = point.distance(point2);
+						if(dist > lineLength) {
+							lineLength = dist;
+							equationStart = point;
+							equationEnd = point2;
+						}
+					}
+				}
+			}
+			candidateTotalLength += lineLength;
+			equationLength = candidateTotalLength;
+			return equationLength;
+		}
 		public String toString() {
 			return x + "," + y;
 		}
 	}
 	/**
 	 * Solve the homestead problem if we use a star.
+	 * We only search in the bounding rectangle
+	 * This search is exhaustive within the rectangle
 	 */
 	public void solveMid() {
 		Point minimum = null;
@@ -67,11 +120,7 @@ class Main {
 		for (double x = minX; x <= maxX; x += STEP) {
 			for (double y = minY; y <= maxY; y += STEP) {
 				Point candidate = new Point(x, y);
-				float candidateTotalLength = 0;
-				for (int i = 0; i < xPoints.length; i++) {
-					candidateTotalLength += new Point(xPoints[i], yPoints[i])
-							.distance(candidate);
-				}
+				double candidateTotalLength = candidate.findStarLength();
 				if (candidateTotalLength < totalLengthMin) {
 					totalLengthMin = candidateTotalLength;
 					minimum = candidate;
@@ -87,61 +136,169 @@ class Main {
 	public void solveEquation() {
 		Point minimum = null;
 		double totalLengthMin = Double.MAX_VALUE;
-		Point start = null;
-		Point end = null;
-		for (double m = -500; m <= 500; m += 1) {
-			for (double c = -3000; c <= 3000; c += 1) {
-				// candidate is a line
-				// it's length is unkown...
-				Point candidate = new Point(m, c);
-				float candidateTotalLength = 0;
-				ArrayList<Point> points = new ArrayList<Point>(5);
-				for (int i = 0; i < xPoints.length; i++) {
-					double x1 = xPoints[i];
-					double y1 = yPoints[i];
-					candidateTotalLength += new Point(x1, y1)
-							.distanceToLine(candidate);
-					// find where these perpendicular points meet the line
-					points.add(new Point((m * y1 + x1 - m*c) / (m*m + 1),(m*m * y1 + m*x1 + c) / (m*m + 1)));
-				}
-				// Add the greatest distance between the points
-				Point candidateStart = null;
-				Point candidateEnd = null;
-				double lineLength = Double.MIN_VALUE;
-				for (Point point : points) {
-					for (Point point2 : points) {
-						double dist = point.distance(point2);
-						if(dist > lineLength) {
-							lineLength = dist;
-							candidateEnd = point;
-							candidateStart = point2;
-						}
-					}
-				}
-				candidateTotalLength += lineLength;
+		
+		for (double x = -500; x <= 500; x += 1) {
+			for (double y = -3000 ; y <= 3000; y += 1) {
+				Point candidate = new Point(x, y);
+				double candidateTotalLength = candidate.findEquationLength();
 				if (candidateTotalLength < totalLengthMin) {
 					totalLengthMin = candidateTotalLength;
 					minimum = candidate;
-					end = candidateEnd;
-					start = candidateStart;
 				}
 			}
 		}
 		System.out.println(String.format(
-				"Minimum line is : %fx+%f with total length %f from "+ start+" to "+end, minimum.x,
-				minimum.y, totalLengthMin));
+				"Minimum line is : %fx+%f with total length %f from %s to %s", minimum.x,
+				minimum.y, minimum.equationLength,minimum.equationStart,minimum.equationEnd));
+	}
+	/**
+	 * This method solves the star problem using the hill climbing method
+	 * I wrote it after I had submitted the assignment
+	 */
+	public void solvemidHillClimbing() {
+		// Take a guess at where the minimum is
+		Point minimum = new Point(0d,0d);
+		while(true) {
+			// find the length of the points around the current
+			ArrayList<Point> surroundingPoints = new ArrayList<>(8);
+			double x = minimum.x;
+			double y = minimum.y;
+			surroundingPoints.add(new Point(x+STEP, y));
+			surroundingPoints.add(new Point(x-STEP, y));
+			surroundingPoints.add(new Point(x+STEP, y+STEP));
+			surroundingPoints.add(new Point(x+STEP, y-STEP));
+			surroundingPoints.add(new Point(x-STEP, y+STEP));
+			surroundingPoints.add(new Point(x-STEP, y-STEP));
+			surroundingPoints.add(new Point(x, y+STEP));
+			surroundingPoints.add(new Point(x, y-STEP));
+			for (Point point : surroundingPoints) {
+				point.findStarLength();
+			}
+			
+			Point candidateMinimum = Collections.min(surroundingPoints, new Comparator<Point>() {
+				@Override
+				public int compare(Point o1, Point o2) {
+					return Double.compare(o1.starLength, o2.starLength);
+				}
+			});
+			// If the current length is the less than those around it we have found the minimum
+			if(minimum.findStarLength() < candidateMinimum.starLength) {
+				break;
+			}
+			// otherwise we repeat with the minimum of the surrounding
+			minimum = candidateMinimum;
+		}
+		System.out.println(String.format(
+				"Minimum point is : %fx+%f with total length %f", minimum.x,minimum.y, minimum.starLength));
+	}
+	/**
+	 * This method solves the line problem using the hill climbing method
+	 * I wrote it after I had submitted the assignment.
+	 * 
+	 *  This should be implemented using simulated 
+	 * annealing to attempt to find a global minimum
+	 * http://en.wikipedia.org/wiki/Simulated_annealing
+	 * 
+	 * This will perform a random walk, once it has found a local minimum
+	 * It will 'jump' off somewhere and see if that leads to another local minimum
+	 */
+	public void solveEquationHillClimbing() {
+		// Take a guess at where the minimum is
+		Point minimum = new Point(20d,-10d);
+		int kMax =  100;
+		int k = 0;
+		ArrayList<Point> localMinima = new ArrayList<Point>(kMax);
+		Comparator<Point> cmp = new Comparator<Point>() {
+			@Override
+			public int compare(Point o1, Point o2) {
+				return Double.compare(o1.equationLength, o2.equationLength);
+			}
+		};
+		while(true) {
+			// find the length of the points around the current
+			ArrayList<Point> surroundingPoints = new ArrayList<>(8);
+			double x = minimum.x;
+			double y = minimum.y;
+			surroundingPoints.add(new Point(x+STEP, y));
+			surroundingPoints.add(new Point(x-STEP, y));
+			surroundingPoints.add(new Point(x+STEP, y+STEP));
+			surroundingPoints.add(new Point(x+STEP, y-STEP));
+			surroundingPoints.add(new Point(x-STEP, y+STEP));
+			surroundingPoints.add(new Point(x-STEP, y-STEP));
+			surroundingPoints.add(new Point(x, y+STEP));
+			surroundingPoints.add(new Point(x, y-STEP));
+			for (Point point : surroundingPoints) {
+				point.findEquationLength();				
+			}
+			// If the current length is the less than those around it
+			// we have found the minimum
+			Point candidateMinimum = Collections.min(surroundingPoints, cmp);
+			if(minimum.findEquationLength() < candidateMinimum.equationLength) {
+				// Repeat this process kMax times 
+				if(k > kMax) {
+					break;
+				} else {
+					k++;
+					localMinima.add(minimum);
+					// Perform a random jump somewhere else in the vicinity
+					minimum = new Point((double)randIntRange(-100, 100),(double)randIntRange(-100, 100));
+				}
+				
+			} else {
+				// otherwise we repeat with the minimum of the surrounding
+				minimum = candidateMinimum;
+			}
+		}
+		// find the global minimum from our kMax attempts
+		minimum = Collections.min(localMinima,cmp);
+		System.out.println(String.format(
+				"Minimum line is : %fx+%f with total length %f from %s to %s", minimum.x,
+				minimum.y, minimum.equationLength,minimum.equationStart,minimum.equationEnd));
+	}
+	private int randIntRange(int min, int max) {
+		return rand.nextInt((max - min) + 1) + min;
+	}
+	/**
+	 * Attempt to solve the line equation using simulated annealing
+	 */
+	private void solveEquationSimulatedAnnealing() {
+		Point minimum = null;
+		int kMax = 50;
+		int k = 0;
+		double e;
+		double eMax;
+		//while(k >kMax && e > eMax) {
+			//double 
+		//}
+		System.out.println(String.format(
+				"Minimum line is : %fx+%f with total length %f from %s to %s", minimum.x,
+				minimum.y, minimum.equationLength,minimum.equationStart,minimum.equationEnd));
 	}
 
 	public static void main(String[] args) {
 		long start = System.nanoTime();
 		Main m = new Main();
 		m.setMax();
+		m.solvemidHillClimbing();
+		System.out.println("\tStar Hill climbing: Found in "
+				+ ((System.nanoTime() - start) / 1000000000f));
+	
+		start = System.nanoTime();
+		m.solveEquationHillClimbing();
+		System.out.println("\tEquation hill climbing: Found in "
+				+ ((System.nanoTime() - start) / 1000000000f));
+		start = System.nanoTime();
+		//m.solveEquationSimulatedAnnealing();
+		//System.out.println("\tEquation simulated annealing: Found in "
+		//		+ ((System.nanoTime() - start) / 1000000000f));
+		start = System.nanoTime();
 		m.solveMid();
-		System.out.println("Found in "
+		System.out.println("\tStar: Found in "
 				+ ((System.nanoTime() - start) / 1000000000f));
 		start = System.nanoTime();
 		m.solveEquation();
-		System.out.println("Found in "
+		System.out.println("\tEquation: Found in "
 				+ ((System.nanoTime() - start) / 1000000000f));
 	}
+
 }
