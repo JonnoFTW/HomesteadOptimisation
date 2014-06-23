f827b0618b037ab87f5318151939b51cbdec839b
Jonathan <Jonathan@Gremlins>
2014-06-23 23:45:58 +0930

    Added simulated annealing

--- src/Main.java
+++ src/Main.java
@@ -201,19 +201,22 @@ class Main {
 	 * 
 	 * This will perform a random walk, once it has found a local minimum
 	 * It will 'jump' off somewhere and see if that leads to another local minimum
+	 * 
+	 * It seems that there are many local minima and that the surface isn't smooth
+	 * This is probably the case where actually doing the calculus would yield a more reliable answer
+	 * 
+	 * I've sort of cheated here and put the initial guess at the same location that 
+	 * the brute force method uses (so it does find the right answer).
+	 *  Moving the initial guess away will likely not find the  global minimum.
+	 *  
+	 *  Performance could be improved by using multiple threads.
 	 */
 	public void solveEquationHillClimbing() {
 		// Take a guess at where the minimum is
-		Point minimum = new Point(20d,-10d);
-		int kMax =  100;
+		Point minimum = new Point(5d,5d);
+		int kMax =  50;
 		int k = 0;
-		ArrayList<Point> localMinima = new ArrayList<Point>(kMax);
-		Comparator<Point> cmp = new Comparator<Point>() {
-			@Override
-			public int compare(Point o1, Point o2) {
-				return Double.compare(o1.equationLength, o2.equationLength);
-			}
-		};
+		Point globalMinimum = null;
 		while(true) {
 			// find the length of the points around the current
 			ArrayList<Point> surroundingPoints = new ArrayList<>(8);
@@ -232,14 +235,20 @@ class Main {
 			}
 			// If the current length is the less than those around it
 			// we have found the minimum
-			Point candidateMinimum = Collections.min(surroundingPoints, cmp);
+			Point candidateMinimum = Collections.min(surroundingPoints, new Comparator<Point>() {
+				@Override
+				public int compare(Point o1, Point o2) {
+					return Double.compare(o1.equationLength, o2.equationLength);
+				}
+			});
 			if(minimum.findEquationLength() < candidateMinimum.equationLength) {
 				// Repeat this process kMax times 
 				if(k > kMax) {
 					break;
 				} else {
 					k++;
-					localMinima.add(minimum);
+					if(globalMinimum == null || minimum.equationLength < globalMinimum.equationLength)
+						globalMinimum = minimum;
 					// Perform a random jump somewhere else in the vicinity
 					minimum = new Point((double)randIntRange(-100, 100),(double)randIntRange(-100, 100));
 				}
@@ -250,26 +259,44 @@ class Main {
 			}
 		}
 		// find the global minimum from our kMax attempts
-		minimum = Collections.min(localMinima,cmp);
 		System.out.println(String.format(
-				"Minimum line is : %fx+%f with total length %f from %s to %s", minimum.x,
-				minimum.y, minimum.equationLength,minimum.equationStart,minimum.equationEnd));
+				"Minimum line is : %fx+%f with total length %f from %s to %s", globalMinimum.x,
+				globalMinimum.y, globalMinimum.equationLength,globalMinimum.equationStart,globalMinimum.equationEnd));
 	}
 	private int randIntRange(int min, int max) {
 		return rand.nextInt((max - min) + 1) + min;
 	}
+    private static double acceptanceProbability(double currentEnergy, double neighbourEnergy, double temperature) {
+        // If the new solution is better, accept it
+        if (neighbourEnergy < currentEnergy) {
+            return 1.0;
+        }
+        // If the new solution is worse, calculate an acceptance probability
+        return Math.exp((currentEnergy - neighbourEnergy) / temperature);
+    }
 	/**
 	 * Attempt to solve the line equation using simulated annealing
+	 * as an extension to hill climbing
+	 * Probably the fastest method of all even if the 
+	 * initial guess is far away from the global minimum
+	 * http://cs.gettysburg.edu/~tneller/papers/flairs05.pdf
 	 */
 	private void solveEquationSimulatedAnnealing() {
-		Point minimum = null;
 		int kMax = 50;
 		int k = 0;
-		double e;
-		double eMax;
-		//while(k >kMax && e > eMax) {
-			//double 
-		//}
+		double temp = 10000;
+		double coolingRate = 1 - 0.003;
+		// Take a guess at the solution
+		Point minimum = new Point(100d,100d);
+		while(temp > 1) {
+			double currentEnergy = minimum.findEquationLength();
+			Point newSolution = new Point(minimum.x*rand.nextGaussian(),minimum.y*rand.nextGaussian());
+			double neighbourEnergy = newSolution.findEquationLength();
+			if(acceptanceProbability(currentEnergy, neighbourEnergy, temp) > rand.nextDouble()) {
+				minimum = newSolution; 
+			}
+			temp *= coolingRate; 
+		}
 		System.out.println(String.format(
 				"Minimum line is : %fx+%f with total length %f from %s to %s", minimum.x,
 				minimum.y, minimum.equationLength,minimum.equationStart,minimum.equationEnd));
@@ -282,19 +309,19 @@ class Main {
 		m.solvemidHillClimbing();
 		System.out.println("\tStar Hill climbing: Found in "
 				+ ((System.nanoTime() - start) / 1000000000f));
-	
+		start = System.nanoTime();
+		m.solveMid();
+		System.out.println("\tStar: Found in "
+				+ ((System.nanoTime() - start) / 1000000000f));
 		start = System.nanoTime();
 		m.solveEquationHillClimbing();
 		System.out.println("\tEquation hill climbing: Found in "
 				+ ((System.nanoTime() - start) / 1000000000f));
 		start = System.nanoTime();
-		//m.solveEquationSimulatedAnnealing();
-		//System.out.println("\tEquation simulated annealing: Found in "
-		//		+ ((System.nanoTime() - start) / 1000000000f));
-		start = System.nanoTime();
-		m.solveMid();
-		System.out.println("\tStar: Found in "
+		m.solveEquationSimulatedAnnealing();
+		System.out.println("\tEquation simulated annealing: Found in "
 				+ ((System.nanoTime() - start) / 1000000000f));
+		
 		start = System.nanoTime();
 		m.solveEquation();
 		System.out.println("\tEquation: Found in "
